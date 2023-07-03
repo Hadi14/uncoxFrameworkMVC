b5ee591d23bdde381aa9c22ce7401e2327344fb1
Hadi <asusn43@yahoo.com>
2023-07-03 21:13:54 +0330

    add login and register methods

--- controller/user.php
+++ controller/user.php
@@ -10,4 +10,69 @@ class UserController
         echo "<br> profile Method.!!! <br>";
         print_r($params);
     }
+    public function login()
+    {
+
+        if (isset($_GET['uname'])) {
+            $this->loginCheck();
+        } else {
+            $this->loginForm();
+        }
+    }
+    private function loginCheck()
+    {
+        if (isset($_GET['submit'])) {
+            require_once("./Config/main.php");
+
+            $db = Db::getInstance();
+            $un = $_GET['uname'];
+            $record = $db->first("select * from users where user='$un'");
+
+            if ($record && $record['password'] == $_GET['pass']) {
+
+                header("Location: index.php");
+            } else {
+                echo "Fail to Login...";
+            }
+        }
+    }
+    private function loginForm()
+    {
+        echo "<br>Login Form.!! Please fill all fields";
+    }
+    public function register()
+    {
+        if (isset($_GET['submit'])) {
+            $this->registerCheck();
+        } else {
+            $this->registerForm();
+        }
+    }
+    private function registerCheck()
+    {
+        require_once("./Config/main.php");
+        if (isset($_GET['submit'])) {
+            $db = Db::getInstance();
+            $year = $_GET['Year']; //
+            $month = $_GET['Month']; //
+            $familycity = $_GET['familycity']; //
+            $familyrural = $_GET['familyrural']; //
+            $familymen = $_GET['familymen']; //
+            $familywomen = $_GET['familywomen']; //
+            $c;
+            // add
+            $record = $db->first("select * from hemayat where Year='$year' and Month='$month'");
+            if ($record) {
+                echo "Already Registered";
+            } else {
+                $us = $_SESSION['suname'];
+                $db->insert("INSERT INTO hemayat (Year,Month,Hmy_CityFamily,Hmy_RuralFamily,Hmy_MenFamily,Hmy_WomenFamily,user) VALUES ('$year','$month','$familycity','$familyrural','$familymen','$familywomen','$us') ");
+                echo "Record Added";
+            }
+        }
+    }
+    private function registerForm()
+    {
+        echo "<br>this page is Register Form";
+    }
 }
